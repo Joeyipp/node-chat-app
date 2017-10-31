0b94d714c2a4efe9e0810f724caf3743de2f7202
Joey Yip <Joey@knoesis.org>
2017-10-31 16:48:44 -0400

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,15 +1,37 @@
+// When you configure SocketIO, you have access to the socket library @ http://localhost:3000/socket.io/socket.io.js
+// That contains all the code that we need for the client to make the connection and to transfer data (client-server || server-client)
+// By loading the socket library to the html page (Scripts), the browser will have access to all sorts of methods available
+
 const path = require('path');
-const express = require('express');
+const http = require('http');
+const express = require('express'); // BTS: express is using a built-in node module called HTTP to create a server
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 // console.log(__dirname + '/../public'); // Old way
 // console.log(publicPath);
 const port = process.env.PORT || 3000;
-
 var app = express();
+var server = http.createServer(app);
+
+// Web Sockets Server
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+// io.on() lets you register an event listener (listen for specific event and doe something when it happens)
+// Web Sockets are PERSISTENT technology (client-server both keep the communication channel opened for as long as they want to)
+// Client socket will try to reconnect if server goes down
+// Connection - built-in events
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+// App.listen() method is calling http.createServer()
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`)
-})
+});
